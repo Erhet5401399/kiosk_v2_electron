bd157db96902e26599251451756e9fdbffdccdd5
Tuvshinjargal <maxximummuziky@gmail.com>
2026-01-28 18:18:14 +0800

    chore: clear up

--- src/renderer/components/service/ServiceCard.tsx
+++ src/renderer/components/service/ServiceCard.tsx
@@ -10,7 +10,7 @@ export function ServiceCard({ service, onSelect }: ServiceCardProps) {
   return (
     <div className="service-card" onClick={() => onSelect(service)}>
       <div className="card-header-flex">
-        <div className="service-icon-box">{service.icon}</div>
+        <div className="service-icon-box small">{service.icon}</div>
         <h3>{service.name}</h3>
       </div>
       <div className="card-content">
